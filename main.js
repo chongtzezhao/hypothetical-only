b7ed9ea2c6941bb68af5597553833b23910f5a27
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2022-01-13 18:36:55 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 window.onload = function(){
     var otterRight = document.getElementById("otter-right");
+    var otterLeft = document.getElementById("otter-left");
     otterRight.animate([
         // keyframes
         { transform: 'translateX(0px)' },
-        { transform: 'translateX(52vw)' },
+        { transform: 'translateX(54vw)' },
         { transform: 'translateX(0px)' }
       ], {
         // timing options
@@ -13,32 +14,24 @@ window.onload = function(){
     setInterval(()=>{
         if(otterRight.style.visibility=="hidden"){
             otterRight.style.visibility = "visible";
+            otterLeft.style.visibility = "hidden";
         }
         else{
+            otterLeft.style.visibility = "visible";
             otterRight.style.visibility = "hidden";
         }
         console.log("visbility changed right")
     }, 3200);
-    var otterLeft = document.getElementById("otter-left");
     otterLeft.animate([
         // keyframes
         { transform: 'translateX(0px)' },
-        { transform: 'translateX(52vw)' },
+        { transform: 'translateX(54vw)' },
         { transform: 'translateX(0px)' }
       ], {
         // timing options
         duration: 6400,
         iterations: Infinity
       });
-    setInterval(()=>{
-        if(otterLeft.style.visibility=="hidden"){
-            otterLeft.style.visibility = "visible";
-        }
-        else{
-            otterLeft.style.visibility = "hidden";
-        }
-        console.log("visbility changed left")
-    }, 3200);
 }
 
 var state = true;
@@ -46,12 +39,15 @@ function changeDetails(){
     var datetime = document.getElementById("datetime-input").value;
     document.getElementById("datetime").innerHTML = datetime;
     var location = document.getElementById("location-input").value;
+    location = location.replaceAll('\n', '<br>')
     document.getElementById("location").innerHTML = location;
     if(state){
-        document.getElementById("form").style.opacity = 0;
+        document.getElementById("form").style.display = "none";
+        document.getElementById("submit").style.opacity = 0;
     }
     else{
-        document.getElementById("form").style.opacity = 1;
+        document.getElementById("form").style.display = "flex";
+        document.getElementById("submit").style.opacity = 1;
     }
     state = !(state);
 }
